20cb84502238feabe4d72a97f755b20cb5dae67a
faizer shaikh <faizershaikh@gmail.com>
2021-07-28 19:02:18 +0530

    updated new code

--- js/main2.js
+++ js/main2.js
@@ -1,26 +1,17 @@
-//import { products, basket } from "./index.js";
-// to add in cart function
-var products = [];
 var cartProd = getCart();
-const getvalue = (arr) => ([...new Set(arr)]);
+const products = [...cartProd.reduce((map, obj) => map.set(obj.productID, obj), new Map()).values()];
+const basketRight = document.querySelector('.bas-right');
+var newTotal = 0;
+var noOfProducts = products.length;
+var newTotal = 0;
+var total = 0;
 
-var array = [0,1,2,1,3,4,5,2,1];
-console.log(getvalue(array));
-
-console.log(bakset)
-fetch("./json/product.json")
-    .then(res => res.json())
-    .then(data => {
-        products = data;
-        products.forEach((product) => {
-            if (product.isInCart === true) {
-                console.log(product);
-            }
-        })
-
-        //data.forEach(product => displayProducts(product))
-    })
-
+// dipslying products to the cart
+products.forEach(product => {
+    addToCart(product);
+});
+displayInvoice();
+// display product function
 function addToCart(product) {
     const card = document.createElement("div");
     card.classList.add("card-dtl");
@@ -32,17 +23,93 @@ function addToCart(product) {
         <div class="content">
             <h2 class="product-heading clr-light">${product.name}</h2>
             <p class="product-discript-dtl clr-light" >${product.discrip}</p>
-            <div class="input-wrapper"><input type="number" class="input clr-light" value="1" min="1" max="50" placeholder="Quantity"></div>
+            <div class="input-wrapper"><input type="number" class="input clr-light" value="1" min="1" max="50" placeholder="Quantity" id="quantity"></div>
             <div class="price-tag">
-                <h2 class="price clr-light">&euro;100/-</h2>
-                <a class="btn-rm btn-sec">&nbsp;Remove</a>
+                <h2 class="price clr-light">&euro;<span id="cardPrice">${product.price}</span></h2>
+                <a class="btn-rm btn-sec" id="${product.productID}">&nbsp;Remove</a>
             </div>
         </div>`;
-    const basketRight = document.querySelector('.bas-right');
     basketRight.appendChild(card);
 }
 
+// displying info to the invoice 
+function displayInvoice() {
+    let cart = getCart();
+    cart.forEach((product) => {
+        total = product.price + total;
+    })
+    document.getElementById("price").innerHTML = `&euro; ${total}`;
+    document.getElementById("total").innerHTML = `&euro; ${total}`;
+    document.getElementById("noOfProd").innerHTML = noOfProducts;
+    newTotal = total;
+}
 
+// udating invoice info on remove
+
+function updateInvoice(key) {
+    total -= key;
+    document.getElementById("price").innerHTML = `&euro; ${total}`;
+    document.getElementById("total").innerHTML = `&euro; ${total}`;
+    noOfProducts--;
+    document.getElementById("noOfProd").innerHTML = noOfProducts;
+}
+
+// update on quanity chenge 
+
+function updateOnQuantity(ele, price) {
+    let quantity = parseInt(ele);
+    //console.log(quantity)
+    if (quantity === 2) {
+        newTotal = ((newTotal - price) + (price * quantity));
+        console.log("inside first if");
+        document.getElementById("price").innerHTML = `&euro; ${newTotal}`;
+        document.getElementById("total").innerHTML = `&euro; ${newTotal}`;
+    } else if (quantity >= 3) {
+        console.log(quantity)
+        newTotal = ((newTotal - (price * (quantity-1))) + (price * quantity))
+        console.log("inside sec if", newTotal, total);
+        document.getElementById("price").innerHTML = `&euro; ${newTotal}`;
+        document.getElementById("total").innerHTML = `&euro; ${newTotal}`;
+        newTotal = total;
+    }
+}
+
+
+document.querySelector(".bas-right").addEventListener("input", (e) => {
+    e.preventDefault();
+    if (e.target.classList.contains("input")) {
+        let price = parseInt(e.target.parentElement.nextElementSibling.firstElementChild.lastElementChild.textContent);
+        if (e.target.value === "") {
+            document.getElementById("price").innerHTML = `&euro; ${total}`;
+            document.getElementById("total").innerHTML = `&euro; ${total}`;
+        } else {
+            updateOnQuantity(e.target.value, price);
+        }
+    }
+})
+// removing products 
+
+basketRight.addEventListener('click', (e) => {
+    if (e.target.classList.contains("btn-rm")) {
+        e.target.parentElement.parentElement.parentElement.remove();
+        removeProduct(parseInt(e.target.id));
+        updateInvoice(e.target.previousElementSibling.lastElementChild.textContent);
+        console.log(total);
+    }
+})
+
+function removeProduct(prod) {
+    const cart = getCart();
+
+    cart.forEach((product, index) => {
+        if (product.productID === prod) {
+            cart.splice(index, 1);
+        }
+    });
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+// getting products from local Storage
 function getCart() {
     let cart;
     if (localStorage.getItem('cart') === null) {
@@ -53,3 +120,4 @@ function getCart() {
 
     return cart;
 }
+
